6a9c224fc3d7ad6f2f6b2fb8ff7808e41a89ba9f
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-04-27 15:58:13 +0430

    Add header

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -17,12 +17,49 @@ const BackFilter = styled.div`
     background-color:rgb(16, 12, 12,.8);
 `
 
+const HeaderContainer = styled.div`
+    display:flex;
+    justify-content:space-between;
+`
+const Logo = styled.div`
+    flex:1;
+    height:100px;
+`
+const Menu = styled.div`
+    display:flex;
+    flex:1;
+    justify-content:flex-end;
+
+    padding:0 20px;
+    h4{
+        font-size:14px;
+        font-weight:600;
+        color:#fff;
+        margin-left:35px;
+        transition:all 200ms ease-in-out;
+        &:hover{ 
+            cursor: pointer;
+            opacity:.8;
+        }
+    }
+`
+
 function HomePage() {
     return (
         <>
            <TopSectionContainer>
                 <BackFilter>
-
+                    <HeaderContainer>
+                        <Logo />
+                        <Menu>
+                            <h4>Home</h4>
+                            <h4>About</h4>
+                            <h4>Projects</h4>
+                            <h4>Skills</h4>
+                            <h4>Tools</h4>
+                            <h4>Contract</h4>
+                        </Menu>
+                    </HeaderContainer>
                 </BackFilter>
             </TopSectionContainer>   
         </>
