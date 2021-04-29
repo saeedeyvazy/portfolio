0cb5ffb51c02448c29910d199a3c86413bc3138e
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-04-29 09:49:22 +0430

    Add devices size list

--- src/components/SkillCard/index.jsx
+++ src/components/SkillCard/index.jsx
@@ -1,34 +1,34 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import styled from 'styled-components'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
+import styled from "styled-components"
 
 const Container = styled.div`
-    display: flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    height:fit-content;
-    width:25%;
-    svg{
-        color:#06BC9B;
-        font-size:4rem;
-    }
-    h3{
-        margin-top: 0;
-        margin-bottom: 5px;
-        font-weight: 400;
-        color: #333;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: fit-content;
+  width: 25%;
+  svg {
+    color: #06bc9b;
+    font-size: 4rem;
+  }
+  h3 {
+    margin-top: 0;
+    margin-bottom: 5px;
+    font-weight: 400;
+    color: #333;
+  }
 `
 
-function SkillCard({icon, title, description}) {
-    return (
-        <Container>
-             <FontAwesomeIcon icon={icon} />   
-             <h3>{title}</h3>
-             <p>{description}</p>
-        </Container>
-    )
+function SkillCard({ icon, title, description }) {
+  return (
+    <Container>
+      <FontAwesomeIcon icon={icon} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </Container>
+  )
 }
 
 export default SkillCard
